9da3d15c6d25d4bd97aa690f6d7108efd203d03e
Victor Castro <victor.castro34@gmail.com>
2024-03-19 12:13:46 -0300

    feat: added products

--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,10 @@ import cakeImg from "../public/images/cakeImg.jpg";
 import paveImg from "../public/images/paveImg.jpg";
 import chocolatePuddingImg from "../public/images/chocolatePuddingImg.jpg";
 import blueberryCakeImg from "../public/images/blueberryCakeImg.jpg";
+import brigadeiroDeOreo from "../public/images/brigadeiroDeOreoImg.jpg";
+import alfajor from "../public/images/alfajorImg.jpg";
+import palhaItaliana from "../public/images/palhaItalianaImg.jpg";
+import cookie from "../public/images/cookieImg.jpg";
 
 export const dessertImages = [
   {
@@ -23,5 +27,32 @@ export const dessertImages = [
     name: "blueberry cake",
     src: blueberryCakeImg,
     alt: "blueberry cake",
+  },
+];
+
+export const products = [
+  {
+    name: "brigadeiro com oreo",
+    price: 5.0,
+    description: "Brigadeiro com oreo é uma delícia",
+    image: brigadeiroDeOreo,
+  },
+  {
+    name: "alfajor",
+    price: 3.0,
+    description: "Alfajor é uma delícia",
+    image: alfajor,
+  },
+  {
+    name: "palha italiana",
+    price: 4.0,
+    description: "Palha italiana é uma delícia",
+    image: palhaItaliana,
+  },
+  {
+    name: "cookie",
+    price: 2.0,
+    description: "Cookie é uma delícia",
+    image: cookie,
   },
 ];
