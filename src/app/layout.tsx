4e8d60320038f104f34fa588eec00503f09bd15c
Victor Castro <victor.castro34@gmail.com>
2024-03-19 12:14:01 -0300

    feat: deleted footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { NavBar } from "@/components/main-components/NavBar";
-import { Footer } from "@/components/main-components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +21,6 @@ export default function RootLayout({
         <div className="bg-gradient-to-r from-green-300 to-blue-300 h-full">
           <NavBar />
           {children}
-          <Footer />
         </div>
       </body>
     </html>
--- src/components/main-components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-  return (
-    <div className="fixed inset-x-0 bottom-0 border border-gray-900">
-      Footer
-    </div>
-  );
-};
