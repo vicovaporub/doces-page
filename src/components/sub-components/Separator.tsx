aa1dbdd80821f0f859b8733cf0399734339bfc32
Victor Castro <victor.castro34@gmail.com>
2024-03-19 12:14:57 -0300

    feat: added separator

---
+++ src/components/sub-components/Separator.tsx
@@ -0,0 +1,9 @@
+export const Separator = () => {
+  return (
+    <div className="border border-black h-10 justify-center bg-gradient-to-r from-green-300 to-blue-300">
+      <h1 className="text-center font-bold justify-center m-1">
+        Aqui é um separador que pode vir sua logo
+      </h1>
+    </div>
+  );
+};
