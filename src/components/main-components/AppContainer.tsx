e8088775c216bea976b875da885128c143ac565c
Victor Castro <victor.castro34@gmail.com>
2024-03-20 16:13:23 -0300

    feat: added cart container to app page

--- src/components/main-components/AppContainer.tsx
+++ src/components/main-components/AppContainer.tsx
@@ -1,11 +1,14 @@
+import { CartButton } from "../sub-components/CartButton";
 import { Separator } from "../sub-components/Separator";
 import { About } from "./About";
+import { CartContainer } from "./CartContainer";
 import { Products } from "./Products";
 
 export const AppContainer = () => {
   return (
     <div>
       <About />
+      <CartContainer />
       <Separator />
       <Products />
     </div>
