e6752228cdb6ae85e09244f139dcadd5e7863606
Victor Castro <victor.castro34@gmail.com>
2024-03-22 15:25:58 -0300

    added mongodb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <div className="bg-gradient-to-r from-green-300 to-blue-300 h-full">
       <NavBar />
+      <div className="p-5 mt-5 text-center bg-yellow-200 text-yellow-800 text-lg font-bold rounded-lg shadow-md">
+        This page is under construction!!
+      </div>
       <AppContainer />
     </div>
   );
