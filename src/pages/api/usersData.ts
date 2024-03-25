4f7c11e8fee3d6883244001d7f6d9ed87548e7e8
Victor Castro <victor.castro34@gmail.com>
2024-03-25 14:46:02 -0300

    added fetch user data

---
+++ src/pages/api/usersData.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase } from "../../../lib/mongodb";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const client = await connectToDatabase();
+    const db = client.db("doces-carol-users");
+    const collection = db.collection("users");
+
+    if (req.method === "POST") {
+      const data = req.body;
+      const existingUser = await collection.findOne({ phone: data.phone });
+      if (existingUser) {
+        res.status(200).json({ user: existingUser });
+      } else {
+        res.status(404).json({ message: "Usuário não registrado" });
+      }
+    } else {
+      return;
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
