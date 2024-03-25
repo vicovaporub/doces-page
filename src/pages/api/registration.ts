70d7af7ebb8ce3e396d217aab4ee605493a3f2b0
Victor Castro <victor.castro34@gmail.com>
2024-03-25 14:45:49 -0300

    added registration

---
+++ src/pages/api/registration.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase } from "../../../lib/mongodb";
+import { UserType } from "@/types/UserType";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const client = await connectToDatabase();
+    const db = client.db("doces-carol-users");
+    const collection = db.collection("users");
+
+    if (req.method === "POST") {
+      const data = req.body;
+      const existingUser = await collection.findOne({ phone: data.phone });
+      if (existingUser) {
+        res.status(400).json({ message: "User already exists" });
+      } else {
+        const newUser: UserType = {
+          username: data.username,
+          phone: data.phone,
+        };
+        console.log(newUser);
+        await collection.insertOne(newUser);
+        res.status(200).json({ message: "User created" });
+      }
+    } else {
+      return;
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
---
+++ src/types/UserType.ts
@@ -0,0 +1,5 @@
+export interface UserType {
+  _id?: string;
+  username: string;
+  phone: string;
+}
