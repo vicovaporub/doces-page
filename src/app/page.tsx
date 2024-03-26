7e170a7ef0440186bc997f6ab634c4f1f10d4964
Victor Castro <victor.castro34@gmail.com>
2024-03-26 16:48:34 -0300

    created order page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 import { AppContainer } from "@/components/main-components/AppContainer";
-import { NavBar } from "@/components/main-components/NavBar";
-import { useEffect, useState } from "react";
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-r from-green-300 to-blue-300 h-full">
-      <NavBar />
+    <>
       <div className="p-5 mt-5 text-center bg-yellow-200 text-yellow-800 text-lg font-bold rounded-lg shadow-md">
         This page is under construction!!
       </div>
       <AppContainer />
-    </div>
+    </>
   );
 }
---
+++ src/app/pedidos/page.tsx
@@ -0,0 +1,5 @@
+import { Orders } from "@/components/main-components/Orders";
+
+export default function ordersPage() {
+  return <Orders />;
+}
