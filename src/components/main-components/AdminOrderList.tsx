bf2669d8c1f83d22b38be9bcf60852232014c75c
Victor Castro <victor.castro34@gmail.com>
2024-03-27 13:27:33 -0300

    added logic to get orders by phone number for admin

--- src/components/main-components/AdminOrderList.tsx
+++ src/components/main-components/AdminOrderList.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-//TODO - A PAGINA INTEIRA
+import { OrderType } from "@/types/OrderType";
 import { useState } from "react";
 
 export const AdminOrderList = () => {
-  /*  const [allOrders, setAllOrders] = useState([]);
-  const [groupedOrders, setGroupedOrders] = useState({});
+  const [ordersList, setOrdersList] = useState([] as OrderType[]);
 
   const getAllOrders = async () => {
     try {
@@ -15,37 +14,40 @@ export const AdminOrderList = () => {
           "Content-Type": "application/json",
         },
       });
-      const data = await response.json();
-      console.log(data);
-      setAllOrders(data);
-      const groupingOrders = groupOrdersByPhone(data);
-      setGroupedOrders(groupingOrders);
-      console.log(groupedOrders);
+      const orders = await response.json();
+      console.log(orders);
+      return orders;
     } catch (error) {
       console.error("Error:", error);
     }
   };
 
-  const groupOrdersByPhone = (orders: any) => {
-    const groupedOrders: { [key: string]: any[] } = {};
-
-    orders.forEach((order: any) => {
-      const phone = order.phone;
-
-      if (groupedOrders[phone]) {
-        groupedOrders[phone].push(order);
-      } else {
-        groupedOrders[phone] = [order];
+  const groupOrdersByPhoneNumber = (orders: OrderType[]) => {
+    return orders.reduce((acc: { [key: string]: OrderType[] }, order) => {
+      const phoneNumber = order.phone;
+      if (!acc[phoneNumber]) {
+        acc[phoneNumber] = [];
       }
-    });
-
-    return groupedOrders;
+      acc[phoneNumber].push(order);
+      return acc;
+    }, {});
   };
- */
+
+  const test = async () => {
+    try {
+      await getAllOrders().then((orders) => {
+        const groupedOrders = groupOrdersByPhoneNumber(orders);
+        console.log(groupedOrders);
+      });
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div>
       <h1>Admin Order List</h1>
-      <button onClick={() => console.log("click")}>GET</button>
+      <button onClick={test}>GET ORDERS LIST</button>
     </div>
   );
 };
