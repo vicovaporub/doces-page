a083eb066c7f1da8e225539da6f528d775ce2c0b
Victor Castro <victor.castro34@gmail.com>
2024-03-28 18:51:14 -0300

    added new styling

--- src/components/main-components/About.tsx
+++ src/components/main-components/About.tsx
@@ -1,44 +1,43 @@
 import Image from "next/image";
 import { dessertImages } from "../../../lib/data";
+import displayCake from "../../../public/images/displayCake.png";
 
 export const About = () => {
   const dataImgs = dessertImages;
+
   return (
     <>
-      <div className="bg-[url('/images/candyshop.jpg')] bg-cover bg-center flex justify-between">
-        <div className="sm:flex grid w-full sm:w-[100%] justify-center items-center">
+      <div className="flex justify-between ">
+        <div className="flex flex-col  sm:ml-[25vh]  h-[240px] w-[500px]">
+          <h1 className="font-bold pt-[20vh] text-amber-800 text-[40px]">
+            Doces da Carol
+          </h1>
+          <h2 className="py-10">
+            Olá, essa aqui é a página dos asdoces da carol. Esse aqui é só um
+            texto que eu to colocando para encher linguiça ta bom caroline?
+          </h2>
+        </div>
+        <div className="flex flex-col">
+          <Image
+            className="sm:mr-10 sm:mt-16  px-2"
+            src={displayCake}
+            alt="display-cake"
+            width={666}
+            height={481}
+          />
           <div
-            className="bg-white justify-center items-center rounded-xl bg-opacity-75
-           mt-5 mb-5 sm:max-w-[20%] w-[80%] sm:h-60 sm:mt-20 mx-auto"
+            className="sm:h-[201px] sm:w-[666px] bg-orange-300 
+  flex rounded-full m-10 items-center  bg-opacity-20 overflow-x-scroll
+  overflow-y-hidden custom-scrollbar p-2 "
           >
-            <h1 className="text-2xl text-center">About</h1>
-            <p className="m-5">
-              Olá, essa aqui é a página dos asdoces da carol. Esse aqui é só um
-              texto que eu to colocando para encher linguiça ta bom caroline?
-            </p>
-          </div>
-          <div
-            className="bg-white items-center rounded-xl bg-opacity-75
-           mt-5 mb-5 sm:max-w-[50%] w-[80%] mx-auto"
-          >
-            <h1 className="text-center text-2xl m-2">
-              Aqui é foto do teus doce
-            </h1>
-            <div className="grid grid-cols-2 justify-items-center items-center">
-              {dataImgs.map((img) => {
-                return (
-                  <div key={img.name}>
-                    <Image
-                      src={img.src}
-                      alt={img.alt}
-                      className="rounded-full sm:p-3 sm:ml-[5rem] w-20 h-20
-        sm:h-60 sm:w-60 object-cover sm:object-cover
-        sm:object-center m-2"
-                    />
-                  </div>
-                );
-              })}
-            </div>
+            {dataImgs.map((img) => (
+              <Image
+                className="m-2 p-2 rounded-full w-[150px] h-[150px]  object-cover"
+                key={img.name}
+                src={img.src}
+                alt={img.alt}
+              />
+            ))}
           </div>
         </div>
       </div>
