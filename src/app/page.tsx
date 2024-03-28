a7fa35995dc2bd9a8d0b0aa67532127eafc83c2b
Victor Castro <victor.castro34@gmail.com>
2024-03-28 18:51:52 -0300

    fixed construction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ import { AppContainer } from "@/components/main-components/AppContainer";
 export default function Home() {
   return (
     <>
-      <div className="p-5 mt-5 text-center bg-yellow-200 text-yellow-800 text-lg font-bold rounded-lg shadow-md">
-        This page is under construction!!
-      </div>
       <AppContainer />
     </>
   );
