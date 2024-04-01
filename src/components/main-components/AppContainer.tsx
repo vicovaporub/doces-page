d16da1cfe2f30388d9dd2ca406e0171273b76664
Victor Castro <victor.castro34@gmail.com>
2024-04-01 17:44:12 -0300

    added tailwind bg

--- src/components/main-components/AppContainer.tsx
+++ src/components/main-components/AppContainer.tsx
@@ -5,6 +5,7 @@ import { Products } from "./Products";
 export const AppContainer = () => {
   return (
     <div>
+      <div className="bg-orange-100 sm:mt-20 absolute top-[-6rem] left-[30rem] z-[-10] sm:h-[55rem] sm:w-full rounded-full blur-[10rem]"></div>
       <About />
       <Separator />
       <Products />
