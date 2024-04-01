91ef5211f9ba78b8673b4a9f6459ea8bc3eac514
Victor Castro <victor.castro34@gmail.com>
2024-04-01 12:00:35 -0300

    added products link button

--- src/components/main-components/About.tsx
+++ src/components/main-components/About.tsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
 import { dessertImages } from "../../../lib/data";
 import displayCake from "../../../public/images/displayCake.png";
+import Link from "next/link";
 
 export const About = () => {
   const dataImgs = dessertImages;
 
   return (
     <>
-      <div className="flex justify-between ">
-        <div className="flex flex-col  sm:ml-[25vh]  h-[240px] w-[500px]">
+      <div className="flex relative justify-between ">
+        <div
+          className="bg-orange-100 sm:mt-20 absolute top-[-6rem] -z-10 sm:left-[30rem]
+         sm:h-[55rem] sm:w-[100rem] rounded-full blur-[10rem] 
+         "
+        ></div>
+        <div className="flex flex-col items-center  sm:ml-[25vh]  h-[240px] w-[500px]">
           <h1 className="font-bold pt-[20vh] text-amber-800 text-[40px]">
             Doces da Carol
           </h1>
@@ -16,6 +22,20 @@ export const About = () => {
             Olá, essa aqui é a página dos asdoces da carol. Esse aqui é só um
             texto que eu to colocando para encher linguiça ta bom caroline?
           </h2>
+          <div className="flex justify-center gap-20">
+            <Link
+              className="bg-amber-900 text-white w-fit p-2 mt-16 rounded-xl hover:bg-amber-800"
+              href="/products"
+            >
+              Produtos
+            </Link>
+            <Link
+              className="bg-amber-900 text-white w-fit p-2 mt-16 rounded-xl hover:bg-amber-800"
+              href=""
+            >
+              Pedido Personalizado
+            </Link>
+          </div>
         </div>
         <div className="flex flex-col">
           <Image
@@ -26,8 +46,8 @@ export const About = () => {
             height={481}
           />
           <div
-            className="sm:h-[201px] sm:w-[666px] bg-orange-300 
-  flex rounded-full m-10 items-center  bg-opacity-20 overflow-x-scroll
+            className="sm:h-[201px] sm:w-[666px] bg-white
+  flex rounded-full m-10 items-center  bg-opacity-30 overflow-x-scroll
   overflow-y-hidden custom-scrollbar p-2 "
           >
             {dataImgs.map((img) => (
