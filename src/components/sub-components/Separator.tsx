b4e5da6f8b30b922f60d0db65044f5bd60a54c3c
Victor Castro <victor.castro34@gmail.com>
2024-04-01 12:00:49 -0300

    finished separator

--- src/components/sub-components/Separator.tsx
+++ src/components/sub-components/Separator.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+import logo from "../../../public/images/carol_logo.png";
+
 export const Separator = () => {
   return (
-    <div className="border border-black h-10 justify-center bg-gradient-to-r from-green-300 to-blue-300">
-      <h1 className="text-center font-bold justify-center m-1">
-        Aqui é um separador que pode vir sua logo
+    <div className="h-[90px] flex justify-between items-center m-5 relative">
+      <h1 className="text-center ml-[20vw] font-bold justify-center text-amber-900 text-[40px]">
+        Doces
       </h1>
+      <div className="absolute inset-0 flex justify-center items-center mx-20">
+        <Image src={logo} alt="logo" width={100} height={100} />
+      </div>
+      <p className="text-amber-900 max-w-[30vw] mr-[5vw] ">
+        Uma descriçãozinha de como é feito os doces e outras coisas
+      </p>
     </div>
   );
 };
