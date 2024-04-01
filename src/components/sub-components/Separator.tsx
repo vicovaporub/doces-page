69027a53f16493bfd677f666d8970bc734109ceb
Victor Castro <victor.castro34@gmail.com>
2024-04-01 17:42:52 -0300

    added style to separator

--- src/components/sub-components/Separator.tsx
+++ src/components/sub-components/Separator.tsx
@@ -3,11 +3,11 @@ import logo from "../../../public/images/carol_logo.png";
 
 export const Separator = () => {
   return (
-    <div className="h-[90px] flex justify-between items-center m-5 relative">
+    <div className="h-[90px] sm:flex sm:justify-between sm:items-center m-5  mb-[9rem]">
       <h1 className="text-center ml-[20vw] font-bold justify-center text-amber-900 text-[40px]">
         Doces
       </h1>
-      <div className="absolute inset-0 flex justify-center items-center mx-20">
+      <div className=" inset-0 flex justify-center items-center mx-20">
         <Image src={logo} alt="logo" width={100} height={100} />
       </div>
       <p className="text-amber-900 max-w-[30vw] mr-[5vw] ">
