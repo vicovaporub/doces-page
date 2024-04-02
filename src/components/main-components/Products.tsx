98fc9e419dd746b7d04c3555a33f9be791e6aaf5
Victor Castro <victor.castro34@gmail.com>
2024-04-02 18:13:31 -0300

    added new style to mobile

--- src/components/main-components/Products.tsx
+++ src/components/main-components/Products.tsx
@@ -14,26 +14,35 @@ export const Products = () => {
   };
 
   return (
-    <div className="sm:flex justify-center">
-      <div className="grid grid-cols-2 gap-40 justify-center sm:justify-center items-center">
+    <div id="products" className="flex justify-center">
+      <div className="grid sm:grid-cols-2 sm:gap-40 justify-center items-center">
         {products.map((product) => {
           return (
-            <div
-              key={product.name}
-              className="flex flex-col items-center m-4 p-4 gap-4 "
-            >
-              <Image
-                src={product.image}
-                alt={product.name}
-                className="sm:w-80 sm:h-80 w-52 h-52 rounded-full object-cover object-center m-2"
-              />
-              <h1 className="text-2xl font-bold capitalize">{product.name}</h1>
-              <h2 className="text-xl font-bold">
-                R$ {product.price.toFixed(2)}
-              </h2>
-              <p className="text-center">{product.description}</p>
-              <AddToCartButton onClick={() => onAddToCartClick(product)} />
-            </div>
+            <>
+              <div
+                key={product.name}
+                className="flex flex-col items-center sm:m-6 sm:p-6 m-2 p-2 gap-2 sm:gap-4 "
+              >
+                <Image
+                  src={product.image}
+                  alt={product.name}
+                  className="md:w-80 md:h-80 sm:w-64 sm:h-64 w-48 h-48  rounded-full object-cover object-center m-4"
+                />
+                <div className="flex flex-col items-center text-center sm:gap-4">
+                  <h1 className="text-xl font-bold capitalize">
+                    {product.name}
+                  </h1>
+                  <h2 className="text-lg font-bold">
+                    R$ {product.price.toFixed(2)}
+                  </h2>
+                  <p className="sm:text-base text-sm text-center">
+                    {product.description}
+                  </p>
+                </div>
+                <AddToCartButton onClick={() => onAddToCartClick(product)} />
+              </div>
+              <div className="sm:hidden border-b border-gray-300 my-2 "></div>
+            </>
           );
         })}
       </div>
