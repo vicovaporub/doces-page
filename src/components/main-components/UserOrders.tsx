bce1e4a7a2a5984fd130954116bbf8e9ec44065b
Victor Castro <victor.castro34@gmail.com>
2024-04-02 18:13:49 -0300

    added new style for mobile

--- src/components/main-components/UserOrders.tsx
+++ src/components/main-components/UserOrders.tsx
@@ -51,7 +51,7 @@ export const UserOrders = () => {
   }, [isLoading, userOrders, displayedOrders.length]);
 
   return (
-    <div className="max-w-4xl mx-auto p-4">
+    <div className="max-w-4xl mx-auto min-h-[100vh] p-4">
       {isNewOrderPlaced && (
         <h1 className="text-green-500 mb-4 text-center">
           Pedido realizado com sucesso! Confira abaixo seus pedidos:
--- src/components/sub-components/Separator.tsx
+++ src/components/sub-components/Separator.tsx
@@ -3,14 +3,20 @@ import logo from "../../../public/images/carol_logo.png";
 
 export const Separator = () => {
   return (
-    <div className="h-[90px] sm:flex sm:justify-between sm:items-center m-5  mb-[9rem]">
-      <h1 className="text-center ml-[20vw] font-bold justify-center text-amber-900 text-[40px]">
+    <div className="sm:h-[90px] h-fit flex justify-between sm:items-center sm:mt-24 mt-10 mb-10 items-center  sm:mb-[9rem] sm:gap-0 gap-8">
+      <h1 className="text-center sm:ml-[20vw] ml-2 font-bold justify-center text-amber-900 sm:text-[40px]">
         Doces
       </h1>
-      <div className=" inset-0 flex justify-center items-center mx-20">
-        <Image src={logo} alt="logo" width={100} height={100} />
+      <div className=" inset-0 flex justify-center items-center sm:mx-20  h-fit ">
+        <Image
+          src={logo}
+          alt="logo"
+          width={100}
+          height={100}
+          className="w-full h-auto object-cover "
+        />
       </div>
-      <p className="text-amber-900 max-w-[30vw] mr-[5vw] ">
+      <p className="text-amber-900 sm:max-w-[30vw] sm:mr-[5vw] ">
         Uma descriçãozinha de como é feito os doces e outras coisas
       </p>
     </div>
