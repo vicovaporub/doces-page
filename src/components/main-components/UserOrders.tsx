26b236efdf58bb7b8d1c8d30a695d3b4d5b907ec
Victor Castro <victor.castro34@gmail.com>
2024-04-03 12:54:39 -0300

    added login check to access user orders

--- src/components/main-components/UserOrders.tsx
+++ src/components/main-components/UserOrders.tsx
@@ -51,65 +51,76 @@ export const UserOrders = () => {
   }, [isLoading, userOrders, displayedOrders.length]);
 
   return (
-    <div className="max-w-4xl mx-auto min-h-[100vh] p-4">
-      {isNewOrderPlaced && (
-        <h1 className="text-green-500 mb-4 text-center">
-          Pedido realizado com sucesso! Confira abaixo seus pedidos:
-        </h1>
-      )}
-      <h1 className="text-2xl font-bold mb-4 text-center">
-        Olá {user.username}, aqui estão seus pedidos:
-      </h1>
-      {isLoading && <div className="text-center">Carregando...</div>}
-      <div className="flex flex-col gap-4 mt-8">
-        {displayedOrders
-          .map((order) => (
-            <div
-              className="border border-gray-300 rounded-lg p-4 flex flex-wrap  items-start"
-              key={order._id}
-            >
-              <div className="flex-1 pr-4">
-                {order.order.map((product, index) => (
-                  <div key={product.name} className="">
-                    <p className="font-bold capitalize mb-1">{product.name}</p>
-                    <p>Quantidade: {product.quantity}</p>
-                    {index !== order.order.length - 1 && (
-                      <div className="border-b border-gray-300 my-2 "></div>
-                    )}
+    <>
+      {user.isLogged === false ? (
+        <div className="max-w-4xl mx-auto min-h-[90vh] p-4">
+          <h1 className="text-2xl font-bold mt-4 mb-4 text-center">
+            É necessário fazer o login para ver seus pedidos!
+          </h1>
+        </div>
+      ) : (
+        <div className="sm:max-w-4xl sm:mx-auto sm:min-h-[100vh] p-4">
+          {isNewOrderPlaced && (
+            <h1 className="text-green-500 mb-4 text-center">
+              Pedido realizado com sucesso! Confira abaixo seus pedidos:
+            </h1>
+          )}
+          <h1 className="text-2xl font-bold mt-4 mb-4 text-center">
+            Olá {user.username}, aqui estão seus pedidos:
+          </h1>
+          {isLoading && <div className="text-center">Carregando...</div>}
+          <div className="flex flex-col gap-4 mt-8">
+            {displayedOrders
+              .map((order) => (
+                <div
+                  className="border border-gray-300 rounded-lg p-4 flex flex-wrap items-center"
+                  key={order._id}
+                >
+                  <div className="flex-1 pr-4">
+                    {order.order.map((product, index) => (
+                      <div key={product.name} className="">
+                        <p className="font-bold capitalize mb-1">
+                          {product.name}
+                        </p>
+                        <p>Quantidade: {product.quantity}</p>
+                        {index !== order.order.length - 1 && (
+                          <div className="border-b border-gray-300 my-2 "></div>
+                        )}
+                      </div>
+                    ))}
                   </div>
-                ))}
-              </div>
-              <div className="flex flex-col gap-4 justify-center p-4">
-                <p>
-                  <span className="font-bold">Data:</span> {order.date}
-                </p>
-                <p>
-                  <span className="font-bold">Total:</span> R${order.total}
-                </p>
-                <p>
-                  {" "}
-                  <span className="font-bold">Status: </span>
-                  <span
-                    className={
-                      order.status === "pending"
-                        ? "text-yellow-500"
-                        : order.status === "confirmed"
-                        ? "text-green-500"
-                        : "text-red-500"
-                    }
-                  >
-                    {order.status === "pending"
-                      ? "A confirmar"
-                      : order.status === "confirmed"
-                      ? "Confirmado"
-                      : "Cancelado"}
-                  </span>
-                </p>
-              </div>
-            </div>
-          ))
-          .reverse()}
-      </div>
-    </div>
+                  <div className="flex flex-col gap-4 p-4 items-start ">
+                    <p className="text-center sm:text-left">
+                      <span className="font-bold">Data:</span> {order.date}
+                    </p>
+                    <p className="text-center sm:text-left">
+                      <span className="font-bold">Total:</span> R${order.total}
+                    </p>
+                    <p className="text-center sm:text-left">
+                      <span className="font-bold">Status: </span>
+                      <span
+                        className={
+                          order.status === "pending"
+                            ? "text-yellow-500"
+                            : order.status === "confirmed"
+                            ? "text-green-500"
+                            : "text-red-500"
+                        }
+                      >
+                        {order.status === "pending"
+                          ? "A confirmar"
+                          : order.status === "confirmed"
+                          ? "Confirmado"
+                          : "Cancelado"}
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              ))
+              .reverse()}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
